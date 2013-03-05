566556a2fad1f4d1cba4e671f3fbfdbe42f4b6ad
n2burns gamma <burns.nicholas.j@gmail.com>
2013-03-05 00:58:57 -0500

    minor updates from 2nd computer

--- hb_encode.sh
+++ hb_encode.sh
@@ -3,9 +3,30 @@
 #!hb_encode.sh encode everything in sourcedir and put it in destdir
 # solution based on http://www.unix.com/302367123-post2.html
 
-sourcedir="/home/n2burns/videos/tv/Adventure Time - Seasons 1-3 Complete [720p] x264.mp4/Adventure Time - Season 1"
-destdir="/home/n2burns/videos/N1/Adventure Time/Season 01"
-cd "$sourcedir"
-for i in *; do
-  HandBrakeCLI -i "$i" -o "$destdir/${i%.*}.m4v" --preset="Android High"
+#extended pattern matching
+shopt -s extglob
+
+function usage {
+  echo "usage: $0 sourceDir destDir"
+}
+
+if [[ $# = 2 ]]; then
+  sourceDir=$(readlink -f "$1")/
+  destDir=$(readlink -f "$2")/
+else
+  usage
+  exit 0
+fi
+
+
+for i in "$sourceDir"/*; do
+  #name without folder 
+  filename="${i##*/}"
+  #check for folders. If a folder is found, recurse on that folder
+  if [ -d "$i" ]; then
+    mkdir "$destDir"/"$filename"
+    ./$0 "$i" "$destDir"/"$filename"
+  else
+    HandBrakeCLI -i "$i" -o "$destDir/${filename%.*}.m4v" --preset="Android High"
+  fi
 done
