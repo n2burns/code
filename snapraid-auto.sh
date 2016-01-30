b0c9026635cb03e3bd05a39a1b2825e3bca141ea
n2burns <burns.nicholas.j@gmail.com>
2016-01-30 15:17:17 -0600

    Create snapraid-auto.sh

---
+++ snapraid-auto.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# snapraid-auto.sh
+
+# performs shutdown of VM gamma, runs snapraid sync
+# (and scrub is run if requested), boots VM gamma
+# NOTE because snapraid requires root, this must be run as root.
+# There are no sudos in this script
+
+# TODO check running gamma commands as n2burns from root
+# TODO email (plus log) snapraid status, SMART status after running
+# TODO fix email!
+
+function usage {
+	echo "usage: $0 [scrub]"
+}
+if [ $# = 0 ]; then
+	su n2burns -c "gamma shutdown"
+	snapraid sync
+	su n2burns -c "gamma boot"
+elif [ $# = 1 ] && [ $1 == "scrub" ]; then
+	su n2burns -c "gamma shutdown"
+	snapraid sync
+	snapraid scrub
+	su n2burns -c "gamma boot"
+else
+	usage
+	exit 0
+fi
