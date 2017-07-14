ee68e188746fc027a2428b88bcb9c7f1615bcbc0
Nicholas Burns <burns.nicholas.j@gmail.com>
2017-07-14 19:39:59 -0400

    Minor

--- music/flac2opus.sh
+++ music/flac2opus.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
+
 #flac2opus.sh
 
 #This script is designed to keep an up-to-date downscale of all of the flac
-#in a library, in ogg format
+#in a library, in opus format. It is modified from an earlier script called
+#flac2ogg.sh
 #
 #It itterates through all the files in the flac folder and sees if a
-#matching file exists in the ogg folder. If not, it creates a new ogg file using the supplied quality
+#matching file exists in the opus folder. If not, it creates a new opus file using the supplied quality
 
-#quality - oggenc default is 3 which gives a filesize of approximately 10% of FLAC
-#q=-1 is the lowest possible value giving a filesize of approx 4.5% of FLAC
-#q=0 6.1% of flac
+#TODO highlight to the user errors reported by opusenc
 
-#TODO highlight to the user errors reported by oggenc
+shopt -s extglob
 
 function usage {
-	echo "usage: $0 flacDir oggDir [quality]"
+	echo "usage: $0 flacDir opusDir [bitrate]"
 }
 
 #if args exist, use them. If not, use the supplied data
 if [ $# = 2 ]; then
 	flac="${1%/}"
-	ogg="${2%/}"
-	q="0"
+	opus="${2%/}"
+	q="48.0"
 elif [ $# = 3 ]; then
 	flac="${1%/}"
-	ogg="${2%/}"
+	opus="${2%/}"
 	q="$3"
 else
 	usage
 	exit 0
 fi
-output="flac2ogg-`date +%Y%m%d_%H%M%S`.log"
+output="flac2opus-`date +%Y%m%d_%H%M%S`.log"
 
 #function that itterates through the folders
 function sub {
@@ -39,6 +39,8 @@ function sub {
 
 	for file in "$folder/"*; do
 		if [ -d "$file" ]; then
+			dir="${file##*/}"
+			mkdir "$opus/$dir"
 			sub "$file"
 		else
 			encode "$file"
@@ -52,22 +54,23 @@ function encode {
 	f="$1"
 	file="${f##$flac}"
 	file="${file##/}"
-	if [ -e "$ogg/$file" ]; then
-		echo "$ogg/$file already exists"
+	if [ -e "$opus/$file" ]; then
+		echo "$opus/$file already exists"
 	elif [[ "$file" == *.flac ]]; then
-		file="${file%.flac}"".ogg"
-		if [ -e "$ogg/$file" ]; then
-			echo "$ogg/$file already exists"
+		file="${file%.flac}"".opus"
+		if [ -e "$opus/$file" ]; then
+			echo "$opus/$file already exists"
 		else
 			#had to set frequency
-			oggenc "$1" -q $q -o "$ogg/$file" --resample 44100
-			if [ ! -e "$ogg/$file" ]; then
-				echo "$ogg/$file was not written" >>  "$output"
+			#looks like it's unneccessary with opusenc
+			opusenc --bitrate "$q" --vbr "$1" "$opus/$file" #--resample 44100
+			if [ ! -e "$opus/$file" ]; then
+				echo "$opus/$file was not written" >>  "$output"
 			fi	
 		fi
 	#copy album art
 	elif [[ "$file" == *.jpg ]]; then
-		cp "$1" "$ogg/$file"
+		cp "$1" "$opus/$file"
 	fi
 }
 
